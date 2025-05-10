8256256859a4d0bfe6bbb0edd28342c60f79951f
lagamiaSam <167587359+lagamiaSam@users.noreply.github.com>
2025-05-11 02:18:00 +0800

    feat(grades page): add bootstrap link and style.css

--- page/index.php
+++ page/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Grades Information</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../page/assets/css/style.css">
     <!-- Bootstrap CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
