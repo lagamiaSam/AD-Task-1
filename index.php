5f125d0c214c2d58528984d67060e062f147f099
lagamiaSam <167587359+lagamiaSam@users.noreply.github.com>
2025-05-11 01:23:20 +0800

    feat(landing page): add off-canvas-menu

--- index.php
+++ index.php
@@ -19,6 +19,31 @@
       aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    
+
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Mallorca Highschool</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">HOME</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">ABOUT US</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="grades.php">GRADES</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">INFO</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
+
 </body>
 </html>
