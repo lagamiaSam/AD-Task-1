22ae67c1430c02e7177fc0337950a73f5455f959
lagamiaSam <167587359+lagamiaSam@users.noreply.github.com>
2025-05-11 02:17:13 +0800

    feat(grades page): add head with boostrap link and style.css

---
+++ page/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Grades Information</title>
+    <link rel="stylesheet" href="style.css">
+    <!-- Bootstrap CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
