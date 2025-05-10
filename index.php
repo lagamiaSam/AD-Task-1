cf818a9c9b76d24a090fc48a73ead728f38dbf37
lagamiaSam <167587359+lagamiaSam@users.noreply.github.com>
2025-05-11 01:20:39 +0800

    feat(landing page): add navigation section

--- index.php
+++ index.php
@@ -8,6 +8,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    
+    <nav class="navbar navbar-expand-lg navbar-light bg-light px-4">
+  <div class="container-fluid">
+    <a class="navbar-brand fw-bold d-flex align-items-center" href="#">
+      <img src="school-logo.png" alt="School Logo" height="60" class="d-inline-block align-text-top">
+      <span class="ms-3 fs-4">Mallorca Highschool</span>
+    </a>
 </body>
 </html>
